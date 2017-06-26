00a75df671822b08419c61f35561eb5a0adb3a85
singuerinc <nahuel.scotti@gmail.com>
2017-06-26 18:21:01 +0200

    works 4

--- src/lines/Line.js
+++ src/lines/Line.js
@@ -1,40 +1,37 @@
-import l3 from "./l3.json";
-
-export default class L3 {
-    constructor() {
-        this._id = "L3";
+export default class Line {
+    constructor(id, data) {
+        this._id = id;
         // console.log(`Line ${this._id} created.`);
         this._color = 0x00C03A;
         this.list = [];
 
-        for(let i=0; i<l3.length; i++){
-            const info = l3[i];
+        for(let i=0; i<data.length; i++){
+            const info = data[i];
             const station = {
                 id: info.id + "-f",
                 name: info.name + "-f",
                 line: info.line,
                 lat: info.lat,
                 lon: info.lon,
+                dir: 1
             };
             this.list.push(station);
         }
 
-        const l3b = l3.reverse();
+        const dataRev = data.reverse();
 
-        for(let i=0; i<l3b.length; i++){
-            const info = l3b[i];
+        for(let i=0; i<dataRev.length; i++){
+            const info = dataRev[i];
             const station = {
                 id: info.id + "-b",
                 name: info.name + "-b",
                 line: info.line,
                 lat: info.lat,
                 lon: info.lon,
+                dir: -1
             };
             this.list.push(station);
         }
-
-
-        // this.list = stations;
     }
 
     set list(value){
