64d7d4797ee815f9c4ca7d6a6ac07507fb866727
singuerinc <nahuel.scotti@gmail.com>
2017-07-08 11:27:04 +0200

    wip

--- src/railway.js
+++ src/railway.js
@@ -1,3 +1,4 @@
+import Train from "./train";
 import WayPoint from "./waypoint";
 import Utils from "./utils";
 
@@ -6,8 +7,21 @@ export default class RailWay extends PIXI.Graphics {
         super();
         // console.log(`RailWay ${id} created ${stations.size}.`);
         this._id = id;
-        // this._color = color;
-        this._color = 0x444444;
+        this._color = color;
+        // this._color = 0x444444;
+
+        this.layerStations = new PIXI.Graphics();
+        this.addChild(this.layerStations);
+
+        for(var i=0; i<stations.length; i++){
+            this.layerStations.addChild(stations[i]);
+        }
+
+        this.layerWayPoints = new PIXI.Graphics();
+        this.addChild(this.layerWayPoints);
+
+        this.layerTrains = new PIXI.Graphics();
+        this.addChild(this.layerTrains);
 
         this._stops = [];
 
@@ -26,16 +40,31 @@ export default class RailWay extends PIXI.Graphics {
 
                 for (var i = 1; i < numWayPoints; i++) {
                     const percentage = (1 / numWayPoints) * i;
-                    console.log(numWayPoints, percentage);
                     const [x, y] = Utils.midpoint(parentStation.x, parentStation.y, station.x, station.y, percentage);
                     const wp = new WayPoint({ id: `wp-${i}`, position: { x: x, y: y } });
-                    this.addChild(wp);
+                    this.layerWayPoints.addChild(wp);
                     this._stops.push(wp);
                 }
             }
             this._stops.push(station);
             parentStation = station;
         }, this);
+
+        for (let i = 0; i < this.stops.length / 4; i++) {
+            const train = new Train(`${i}`, {
+                stops: this.stops
+            });
+            let stopIndex = i * 4;
+            train.parkIn(this.stops[stopIndex], stopIndex);
+            train.run();
+            this.layerTrains.addChild(train);
+        }
+
+        const f = global.gui.addFolder(this._id);
+        f.add(this, 'visible');
+        f.add(this.layerStations, 'visible');
+        f.add(this.layerWayPoints, 'visible');
+        f.add(this.layerTrains, 'visible');
     }
 
     get stops() {
