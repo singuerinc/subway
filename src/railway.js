bfdce91e0fbe8d16ba4c104bb13d996d3287ad48
singuerinc <nahuel.scotti@gmail.com>
2017-07-14 03:34:55 +0200

    wip

--- src/railway.js
+++ src/railway.js
@@ -1,115 +1,105 @@
 import * as k from 'keymaster';
 import Train from './train';
-import WayPoint from './waypoint';
 import MathUtils from './mathUtils';
-import Station from './station';
+import WayPointSprite from './waypoint.sprite';
+import StationSprite from './station.sprite';
 
 /**
  * @extends PIXI.Graphics
  */
 export default class RailWay extends PIXI.Graphics {
-  constructor({ id, line, idx }) {
-    super();
-    // console.log(`RailWay ${id} created ${stations.size}.`);
-    this._id = id;
-    this._color = line.color;
+    constructor({ id, line }) {
+        super();
+        // console.log(`RailWay ${id} created ${stations.size}.`);
+        this._id = id;
+        this._color = line.color;
 
-    this.layerLines = new PIXI.Graphics();
-    this.layerLines.cacheAsBitmap = true;
-    this.addChild(this.layerLines);
+        this.layerLines = new PIXI.Graphics();
+        this.layerLines.cacheAsBitmap = true;
+        this.addChild(this.layerLines);
 
-    this.layerWayPoints = new PIXI.Graphics();
-    this.addChild(this.layerWayPoints);
+        this.layerWayPoints = new PIXI.Graphics();
+        this.addChild(this.layerWayPoints);
 
-    this.layerStations = new PIXI.Graphics();
-    this.addChild(this.layerStations);
+        this.layerStations = new PIXI.Graphics();
+        this.addChild(this.layerStations);
 
-    const stations = [];
-    for (let i = 0; i < line.list.length; i += 1) {
-      const station = new Station(line.list[i]);
-      stations.push(station);
-      this.layerStations.addChild(station);
-    }
+        const stations = line.onlyStations;
+        // for (let i = 0; i < stations.length; i += 1) {
+        //     const model = stations[i];
+        //     const station = new StationSprite(model);
+        //     this.layerStations.addChild(station);
+        // }
 
-    this.layerTrains = new PIXI.Graphics();
-    this.addChild(this.layerTrains);
+        this.layerTrains = new PIXI.Graphics();
+        this.addChild(this.layerTrains);
+        //
+        this._stops = [];
+        //
+        let parentStation;
 
-    this._stops = [];
+        for (var [key, wayPoint] of line.wayPoints.entries()) {
+            if (wayPoint.type === 1) {
+                console.log(wayPoint);
+                const station = new StationSprite({model: wayPoint, color: line.color, dir: 1});
 
-    let parentStation;
-    stations.forEach((station, a, b) => {
-      if (!parentStation) {
-        // if no parent station means that is the first one,
-        // so the parent station is the last one.
-        parentStation = b[b.length - 1];
-      }
+                this.layerStations.addChild(station);
 
-      const px = parentStation.x;
-      const py = parentStation.y;
-      const sx = station.x;
-      const sy = station.y;
+                if (parentStation) {
+                    const px = parentStation.position.x;
+                    const py = parentStation.position.y;
+                    const sx = wayPoint.position.x;
+                    const sy = wayPoint.position.y;
 
-      if (station.dir === 1) {
-        this.layerLines.lineStyle(66, this._color, 0.1);
-        this.layerLines.moveTo(px, py);
-        this.layerLines.lineTo(sx, sy);
-        this.layerLines.lineStyle(5, this._color, 1);
-        this.layerLines.moveTo(px, py);
-        this.layerLines.lineTo(sx, sy);
-      }
+                    this.layerLines.lineStyle(66, this._color, 0.1);
+                    this.layerLines.moveTo(px, py);
+                    this.layerLines.lineTo(sx, sy);
+                    this.layerLines.lineStyle(5, this._color, 1);
+                    this.layerLines.moveTo(px, py);
+                    this.layerLines.lineTo(sx, sy);
+                }
 
-      // create at least 1 wayPoint between stations
-      // FIXME: numWayPoints min should be 1, not 2
-      const distanceBtwStations = MathUtils.distance(sx, sy, px, py);
-      const numWayPoints = Math.floor(distanceBtwStations / 40);
+                parentStation = wayPoint;
+            } else if (wayPoint.type === 0) {
+                const wp = new WayPointSprite({model: wayPoint, color: line.color, dir: 1});
+                this.layerWayPoints.addChild(wp);
+            }
 
-      let prevStop = station;
-      for (let i = 0; i < numWayPoints - 1; i++) {
-        const percentage = (1 / numWayPoints) * (i + 1);
-        const [x, y] = MathUtils.midpoint(px, py, sx, sy, percentage);
-        const wp = new WayPoint({ id: `${parentStation.id}-wp-${i}`, prevStop, position: { x, y } });
-        if (station.dir === 1) {
-          this.layerWayPoints.addChild(wp);
+            this._stops.push(wayPoint);
         }
-        this._stops.push(wp);
-        prevStop = wp;
-      }
-      station.parentStation = prevStop;
 
-      this._stops.push(station);
-      parentStation = station;
-    }, this);
+        // this._addTrains(Math.floor(stations.length * 0.5));
+        this._addTrains(1);
 
-    this._addTrains(Math.floor(stations.length * 0.5));
-
-    k(`${idx}`, () => {
-      this.visible = !this.visible;
-    });
-
-    k('r', () => {
-      this.layerLines.visible = !this.layerLines.visible;
-    });
-  }
-
-  get stops() {
-    return this._stops;
-  }
-
-  _addTrains(numTrains) {
-    for (let i = 0; i < numTrains; i++) {
-      const train = new Train(`${i}`, {
-        stops: this.stops,
-        color: this._color,
-      });
-      const stopIndex = i * Math.floor(this.stops.length / numTrains);
-
-      train.parkIn(this.stops[stopIndex], stopIndex);
-      train.run();
-      this.layerTrains.addChild(train);
-
-      if (i === 1 && this._id === 'L1') {
-        train.openTrainInfo();
-      }
+        //
+        // k(`${idx}`, () => {
+        //   this.visible = !this.visible;
+        // });
+        //
+        // k('r', () => {
+        //   this.layerLines.visible = !this.layerLines.visible;
+        // });
     }
-  }
+
+    get stops() {
+        return this._stops;
+    }
+
+    _addTrains(numTrains) {
+        for (let i = 0; i < numTrains; i++) {
+            const train = new Train(`${i}`, {
+                stops: this.stops,
+                color: this._color,
+            });
+            const stopIndex = i * Math.floor(this.stops.length / numTrains);
+
+            train.parkIn(this.stops[stopIndex], stopIndex);
+            train.run();
+            this.layerTrains.addChild(train);
+
+            if (i === 1 && this._id === 'L1') {
+                train.openTrainInfo();
+            }
+        }
+    }
 }
---
+++ src/units/waypoint.js
@@ -0,0 +1,105 @@
+import EventEmitter from 'wolfy87-eventemitter';
+
+export default class WayPoint {
+    constructor({ id, name, position }) {
+        this._emitter = new EventEmitter();
+        this._id = id;
+        this._name = name;
+        this._position = position;
+        this._currentTrain = null;
+    }
+
+    /**
+     * @returns {EventEmitter}
+     */
+    get emitter() {
+        return this._emitter;
+    }
+
+    /**
+     * @returns {string}
+     */
+    get id() {
+        return this._id;
+    }
+
+    /**
+     * @returns {string}
+     */
+    get name() {
+        return this._name;
+    }
+
+    /**
+     * @returns {number}
+     */
+    get type() {
+        return 0;
+    }
+
+    /**
+     * @returns {Object}
+     */
+    get position() {
+        return this._position;
+    }
+
+    /**
+     * Indicates that a train wants to reserve this station.
+     * This action should be performed before the entering to the station
+     * to be sure that no train is in the station.
+     * @param {Train} train
+     * @throws Error If a train is currently in this station and can not be reserved.
+     */
+    reserve(train) {
+        if (this._currentTrain === null) {
+            this._currentTrain = train;
+            this._color = 0xFFFF00;
+            this.emitter.emitEvent('train:reserve', [train]);
+        } else {
+            throw new Error('A train is in this station!');
+        }
+    }
+
+    /**
+     * Indicates that a train wants to enter in this station.
+     * @param {Train} train
+     * @throws Error If a train is currently in this station and can not be reserved.
+     */
+    enter(train) {
+        if (this._currentTrain === train) {
+            this._color = 0xFF0000;
+            this.emitter.emitEvent('train:enter', [train]);
+        } else {
+            throw new Error('A train is in this station!');
+        }
+    }
+
+    /**
+     * Indicates that a train is leaving this station.
+     * @param {Train} train
+     */
+    leave(train) {
+        if (train === this._currentTrain) {
+            this._currentTrain = null;
+            this._color = 0x111111;
+            this.emitter.emitEvent('train:reserve', [train]);
+        }
+    }
+
+    /**
+     * The train that is currently in the station
+     * @returns {null|Train}
+     */
+    getCurrentTrain() {
+        return this._currentTrain;
+    }
+
+    /**
+     * Returns if a train is or not in the station
+     * @returns {boolean}
+     */
+    hasTrain() {
+        return this.getCurrentTrain() !== null;
+    }
+}
